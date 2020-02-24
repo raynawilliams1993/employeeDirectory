02b3a79d9203194fda909d5f7469305afd85fff8
rayna williams <Raynawilliams123@gmail.com>
2020-02-23 17:00:35 -0700

    added a search bar with autofill

---
+++ my-app/src/Components/Searchbar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+
+// Using the datalist element we can create autofill suggestions based on the props.employees
+function SearchForm(props) {
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label htmlFor="breed">Employee Name:</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="employee"
+          list="employees"
+          type="text"
+          className="form-control"
+          placeholder="Search a Name to Begin!"
+          id="employee"
+        />
+        <datalist id="employees">
+          {props.breeds.map(employee => (
+            <option value={employee} key={employee} />
+          ))}
+        </datalist>
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
